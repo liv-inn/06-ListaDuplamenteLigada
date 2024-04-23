0ddfa6e3543ae3cc9108af6fb595acda8c0a2a59
liv-inn <liviaavelino2002@gmail.com>
2024-04-23 12:08:05 -0300

    first commit

--- ListaDuplamenteLigada/ListaDuplamenteLigada/ListaDuplamenteLigada.cpp
+++ ListaDuplamenteLigada/ListaDuplamenteLigada/ListaDuplamenteLigada.cpp
@@ -54,7 +54,7 @@ void menu()
 			break;
 		case 2:inserirElemento();
 			break;
-		case 3: exibirQuantidadeElementos(); 
+		case 3: exibirQuantidadeElementos();
 			break;
 		case 4: exibirElementos();
 			break;
@@ -150,17 +150,64 @@ void inserirElemento()
 // funções a serem implementadas no exericio
 void exibirReverso()
 {
-
+	if (primeiro == NULL) {
+		cout << "Lista vazia \n";
+		return;
+	}
+	else {
+		cout << "Elementos: \n";
+		NO* aux = ultimo;
+		while (aux != NULL) {
+			cout << aux->valor << endl;
+			aux = aux->ant;
+		}
+	}
 }
 
 void excluirPrimeiroElemento()
 {
+	if (primeiro == NULL) {
+		cout << "Lista vazia!" << endl;
+		return;
+	}
+
+
+	if (primeiro == ultimo) {
+		free(primeiro);
+		primeiro = NULL;
+		ultimo = NULL;
+		cout << "Elemento excluido, agora a lista esta vazia." << endl;
+		return;
+	}
+
+	NO* aux = primeiro;
+	primeiro = aux->prox;
+	primeiro->ant = NULL;
+	free(aux);
+	cout << "Primeiro elemento excluido com sucesso!" << endl;
 
 }
 
 void excluirUltimoElemento()
 {
+	if (primeiro == NULL) {
+		cout << "Lista vazia!" << endl;
+		return;
+	}
 
+	if (ultimo == primeiro) {
+		free(primeiro);
+		primeiro = NULL;
+		ultimo = NULL;
+		cout << "Elemento excluido, agora a lista esta vazia." << endl;
+		return;
+	}
+
+	NO* aux = ultimo;
+	ultimo = aux->ant;
+	ultimo->prox = NULL;
+	free(aux);
+	cout << "Primeiro elemento excluido com sucesso!" << endl;
 }
 
 
